08d5207a77da7e0b8b5b52add7e538e50174d65d
hitsujiro <hitsujionline@yahoo.co.jp>
2024-12-05 15:41:48 +0900

    コミット

---
+++ my-app/src/app/_document.tsx
@@ -0,0 +1,21 @@
+import { Head, Html, Main, NextScript } from "next/dist/pages/_document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
